e3c7318a897d59f762bfd0eee3fdbddf70c5bca4
95yuki0305 <matsuyuki0305@gmail.com>
2021-05-13 15:40:31 +0900

    FactoryBotの記述を編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
-  validates :family_name_ruby, presence: true
-  validates :first_name_ruby, presence: true
+  validates :family_name_ruby, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_ruby, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    family_name           {Faker::Name.last_name}
+    first_name            {Faker::Name.first_name}
+    family_name_ruby      {Gimei.name.last.katakana}
+    first_name_ruby       {Gimei.name.first.katakana}
+    birthday              {Faker::Date.birthday(min_age: 18, max_age: 90)}
+  end
+end
