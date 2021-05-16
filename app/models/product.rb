f0194f01dfc861ea486bb9bb86dcd355a698836c
95yuki0305 <matsuyuki0305@gmail.com>
2021-05-16 09:28:20 +0900

    各アソシエーションを記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shinpping_charges
+  belongs_to :prefecture
+  belongs_to :days_to_ship
 end
