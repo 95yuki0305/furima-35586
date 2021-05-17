56e141784d2ccebf25f18c66f4d3d17c13a75b85
95yuki0305 <matsuyuki0305@gmail.com>
2021-05-17 18:34:23 +0900

    productモデルのFactoryBotを設定

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :product do
-    
+    name                 {Faker::Commerce.product_name}
+    description          {Faker::Lorem.sentence}
+    category_id          { 2 }
+    status_id            { 2 }
+    shinpping_charge_id  { 2 }
+    prefecture_id        { 2 }
+    days_to_ship_id      { 2 }
+    selling_price        {Faker::Number.number(digits: 4)}
+    association :user
   end
 end
