1aa0be4450d18c8607bda286f1376c0f2e0b6856
95yuki0305 <matsuyuki0305@gmail.com>
2021-05-19 17:12:25 +0900

    show,edit,updateアクションに共通していた変数をメソッド化
    同時にbeforeアクションを定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :set_product, only: [:show, :edit, :update]
+
   def index
     @products = Product.all.order(created_at: :desc)
   end
@@ -19,18 +20,15 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
     unless @product.user == current_user
       redirect_to root_path
     end
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path
     else
@@ -42,5 +40,9 @@ class ProductsController < ApplicationController
   
   def product_params
     params.require(:product).permit(:name, :description, :category_id, :status_id, :shinpping_charge_id, :prefecture_id, :days_to_ship_id, :selling_price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
