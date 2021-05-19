f3a9b17bdb3ebad22859cde3b8884cc2691a13b0
95yuki0305 <matsuyuki0305@gmail.com>
2021-05-19 17:42:40 +0900

    ログイン状態の出品者のみ、出品した商品情報を削除できること

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_product, only: [:show, :edit, :update]
-  before_action :redirect_root, only: [:edit, :update]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :redirect_root, only: [:edit, :update, :destroy]
 
   def index
     @products = Product.all.order(created_at: :desc)
@@ -35,6 +35,9 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    binding.pry
+    @product.destroy
+    redirect_to root_path
   end
 
   private
