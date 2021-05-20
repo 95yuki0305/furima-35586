f17d2f3c92aad6cf43e856688d1f1fcb80fc8534
95yuki0305 <matsuyuki0305@gmail.com>
2021-05-20 22:01:23 +0900

    Formオブジェクト作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,18 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :product_id, :token, :user_id
+
+    with_options presence: true do
+      validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, allow_blank: true}
+      validates :municipality
+      validates :house_number
+      validates :phone_number, format: { with: /\A\d{10,11}\z/, allow_blank: true}
+    end
+    validates :prefecture_id, numericality: { other_than: 1 }
+
+    def save
+      order = Order.create(user_id: user_id, product_id: product_id)
+      Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    end
+  end
+end
